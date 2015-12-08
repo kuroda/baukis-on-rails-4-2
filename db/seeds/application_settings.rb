a9ffad61787115c65cc4b39460f39054b2ca740d
Tsutomu Kuroda <t-kuroda@oiax.jp>
2015-12-08 10:24:58 +0900

    db/seeds/application_settings.rb を簡素化

--- db/seeds/application_settings.rb
+++ db/seeds/application_settings.rb
@@ -1,7 +1,4 @@
-if ActiveRecord::Base.connection.tables.include?('application_settings')
-  ApplicationSetting.count == 0
-  ApplicationSetting.create!(
-    application_name: 'Baukis',
-    session_timeout: 60
-  )
-end
+ApplicationSetting.create!(
+  application_name: 'Baukis',
+  session_timeout: 60
+)
