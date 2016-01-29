d6990fc7feec4ee48853aaa8bed68b2610a55b14
Tsutomu Kuroda <t-kuroda@oiax.jp>
2016-01-29 12:48:11 +0900

    _index.yml にテーブルリストを記述

--- spec/initial_data_loader.rb
+++ spec/initial_data_loader.rb
@@ -8,29 +8,39 @@ end
 
 conn = ActiveRecord::Base.connection
 
-unless conn.table_exists?('_initial_data_digest')
-  conn.create_table('_initial_data_digest') do |t|
+DIGEST_TABLE_NAME = '_initial_data_digest'
+
+unless conn.table_exists?(DIGEST_TABLE_NAME)
+  conn.create_table(DIGEST_TABLE_NAME) do |t|
     t.string :md5_value
   end
 end
 
 class InitialDataDigest < ActiveRecord::Base
-  self.table_name = '_initial_data_digest'
+  self.table_name = DIGEST_TABLE_NAME
 end
 
 digest = InitialDataDigest.first
 
 unless digest.try(:md5_value) == md5.hexdigest
-  table_names = %w(staff_members customers)
-
-  DatabaseCleaner.strategy = :truncation
+  DatabaseCleaner.strategy = :truncation, { except: [ DIGEST_TABLE_NAME ] }
   DatabaseCleaner.clean
 
-  table_names.each do |table_name|
-    path = Rails.root.join('spec', 'initial_data', "#{table_name}.rb")
-    if File.exist?(path)
+  yaml_path = Rails.root.join('spec', 'initial_data', '_index.yml')
+  if File.exist?(yaml_path)
+    table_names = YAML.load_file(yaml_path)
+    table_names.each do |table_name|
+      path = Rails.root.join('spec', 'initial_data', "#{table_name}.rb")
+      if File.exist?(path)
+        puts "Creating #{table_name}...."
+        require path
+      end
+    end
+  else
+    Dir[Rails.root.join("spec/initial_data/*.rb")].each do |f|
+      table_name = f.match(/(\w+)\.rb$/)[1]
       puts "Creating #{table_name}...."
-      require(path)
+      require f
     end
   end
 
